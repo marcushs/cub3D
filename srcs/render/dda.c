169dacf1afe54e84320f9650d65da81856dc60f8
Hok sum Leung <hleung@z1r6p2.42lyon.fr>
2023-10-18 16:24:28 +0200

    raycast math should be correct

--- srcs/render/dda.c
+++ srcs/render/dda.c
@@ -6,11 +6,12 @@
 /*   By: hleung <hleung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 10:20:45 by hleung            #+#    #+#             */
-/*   Updated: 2023/10/18 10:38:00 by hleung           ###   ########.fr       */
+/*   Updated: 2023/10/18 16:16:34 by hleung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3D.h"
+void draw_line_color(t_data *data, float x1, float y1, float x2, float y2, int color);
 
 void	dda(t_data *data)
 {
@@ -22,75 +23,101 @@ void	dda(t_data *data)
 	// t_coordinate	plane;
 	int				step_x;
 	int				step_y;
-	int				hit_axis;
+	int				side;
+	float			perpWallDist;
+	// int				hit_axis;
 	int	map_x;
 	int	map_y;
+	int	ratio;
+	int	pix;
 
 	start.x = data->player->coordinate->x;
 	start.y = data->player->coordinate->y;
 	map_x = start.x;
 	map_y = start.y;
-	dir.x = cos(data->player->angle);
-	dir.y = -(sin(data->player->angle));
-	// plane.x = 0; // sin(data->player->angle);
-	// plane.y = 0.66;
-	if (dir.x == 0)
-		unit_step.x = 1e30;
-	else 
-		unit_step.x = sqrtf(1 + powf(dir.y/dir.x, 2));
-	if (dir.y == 0)
-		unit_step.y = 1e30;
-	else
-		unit_step.y = sqrtf(1 + powf(dir.x/dir.y, 2));
-	if (dir.x < 0)
+	
+	pix = 0;
+	while (pix++ < 128)
 	{
-		step_x = -1;
-		ray_len_1d.x = (start.x - (float)map_x) * unit_step.x;
-		start.x += dir.x;
-	}
-	else
-	{
-		step_x = 1;
-		ray_len_1d.x = ((float)(map_x + 1) - start.x) * unit_step.x;
-		start.x += dir.x;
-	}
-	if (dir.y < 0)
-	{
-		step_y = -1;
-		ray_len_1d.y = (start.y - (float)map_y) * unit_step.y;
-		start.y += dir.y;
-	}
-	else
-	{
-		step_y = 1;
-		ray_len_1d.y = ((float)(map_y + 1) - start.y) * unit_step.y;
-		start.y += dir.y;
-	}
-	// float max_len = 8.0;
-	int	hit = 0;
-	while (!hit && map_x < (data->config->map_row - 1) && map_x >= 0 && map_y < (data->config->map_size - 1) && map_y >= 0)
-	{
-		if (ray_len_1d.x < ray_len_1d.y)
+		ratio = (pix - 64)/ 64;
+		dir.x = cos(data->player->angle) / 2 + cos(data->player->angle - 0.25) * ratio;
+		dir.y = sin(data->player->angle) / 2 + sin(data->player->angle - 0.25) * ratio;
+		if (dir.x == 0)
+			unit_step.x = 1e30;
+		else 
+			unit_step.x = sqrtf(1 + powf(dir.y/dir.x, 2));
+		if (dir.y == 0)
+			unit_step.y = 1e30;
+		else
+			unit_step.y = sqrtf(1 + powf(dir.x/dir.y, 2));
+		if (dir.x < 0)
 		{
-			map_x += step_x;
-			ray_len_1d.x += unit_step.x;
+			step_x = -1;
+			ray_len_1d.x = (start.x - (float)map_x) * unit_step.x;
 			start.x += dir.x;
-			hit_axis = 0;
 		}
 		else
 		{
-			map_y += step_y;
-			ray_len_1d.y += unit_step.y;
-			start.y += dir.y;
-			hit_axis = 1;
+			step_x = 1;
+			ray_len_1d.x = ((float)(map_x + 1) - start.x) * unit_step.x;
+			start.x += dir.x;
 		}
-		if (data->config->map[(int)start.y][(int)start.x] == '1')
+		if (dir.y < 0)
 		{
-			hit = 1;
-			put_pixel(data->mlx, (start.x + 1) * 15, (start.y + 1)  * 15, 0x00FF0000);
-			return ;
+			step_y = -1;
+			ray_len_1d.y = (start.y - (float)map_y) * unit_step.y;
+			start.y += dir.y;
+		}
+		else
+		{
+			step_y = 1;
+			ray_len_1d.y = ((float)(map_y + 1) - start.y) * unit_step.y;
+			start.y += dir.y;
+		}
+		// float max_len = 8.0;
+		int	hit = 0;
+		while (!hit && map_x < (data->config->map_row - 1) && map_x >= 0 && map_y < (data->config->map_size - 1) && map_y >= 0)
+		{
+			if (ray_len_1d.x < ray_len_1d.y)
+			{
+				map_x += step_x;
+				ray_len_1d.x += unit_step.x;
+				// start.x += dir.x;
+				// hit_axis = 0;
+				side = 0;
+			}
+			else
+			{
+				map_y += step_y;
+				ray_len_1d.y += unit_step.y;
+				// start.y += dir.y;
+				// hit_axis = 1;
+				side = 1;
+			}
+			if (data->config->map[map_y][map_x] == '1')
+			{
+				hit = 1;
+				return ;
+			}
+			if (side == 0)
+				perpWallDist = (map_x - data->player->coordinate->x + (1 - step_x) / 2) / dir.x;
+			else
+				perpWallDist = (map_y - data->player->coordinate->y + (1 - step_y) / 2) / dir.y;
+			int	wallcolor = (side == 0) ? 0x0000FFFF : 0x00DD88BB;
+			draw_line_color(data, pix, 64-32/perpWallDist, pix, 64+32/perpWallDist, wallcolor);
 		}
 	}
-	printf("hit axis %d\n", hit_axis);
+	// plane.x = 0; // sin(data->player->angle);
+	// plane.y = 0.66;
 	// data->rays[data->r_idx++] = ray;
+}
+
+void draw_line_color(t_data *data, float x1, float y1, float x2, float y2, int color)
+{
+	for (float i = 0.0f; i < 1.0f; i += .0005f)
+	{
+		int	x = x1 + (x2 - x1) * i;
+		int y = y1 + (y2 - y1) * i;
+		put_pixel(data->mlx->mlx, x, y, color);
+	}
 }
--- srcs/render/render.c
+++ srcs/render/render.c
@@ -6,7 +6,7 @@
 /*   By: hleung <hleung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 18:15:02 by tduprez           #+#    #+#             */
-/*   Updated: 2023/10/18 10:31:28 by hleung           ###   ########.fr       */
+/*   Updated: 2023/10/18 16:20:53 by hleung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void render(t_config *config)
 	init_data(&data, config);
 	mlx_hook(data.mlx->mlx_win, 17, 0, &event_close, data.mlx);
 	mlx_hook(data.mlx->mlx_win, 2, (1L << 0), &event_move, data.mlx);
+	// dda(&data);
 	render_minimap(&data, data.mlx, data.config, data.player->coordinate);
 	mlx_loop(data.mlx->mlx);
 	return;
@@ -29,29 +30,31 @@ void render(t_config *config)
 
 void render_minimap(t_data *data, t_mlx *mlx, t_config *config, t_coordinate* coordinate)
 {
-	int x;
-	int y;
+	// int x;
+	// int y;
+	(void)coordinate;
 
 	config->map_row = map_longest_row(data);
-	mlx->mini_map_img = mlx_new_image(mlx->mlx, (config->map_row + 1) * 15, (config->map_size + 1) * 15);
+	mlx->mini_map_img = mlx_new_image(mlx->mlx, 1900, 950);
+	// mlx->mini_map_img = mlx_new_image(mlx->mlx, (config->map_row + 1) * 15, (config->map_size + 1) * 15);
 	mlx->addr = mlx_get_data_addr(mlx->mini_map_img, &mlx->bits_per_pixel, &mlx->line_length, &mlx->endian);
-	y = 0;
-	while (y < config->map_size)
-	{
-		x = 0;
-		while (config->map[y][x] && config->map[y][x] != '\n')
-		{
-			if ((int)coordinate->y == y && (int)coordinate->x == x)
-				put_square(mlx, coordinate->x, coordinate->y, 1);
-			else if (config->map[y][x] == '1')
-				put_square(mlx, x, y, 0);
-			x++;
-		}
-		y++;
-	}
+	// y = 0;
+	// while (y < config->map_size)
+	// {
+	// 	x = 0;
+	// 	while (config->map[y][x] && config->map[y][x] != '\n')
+	// 	{
+	// 		if ((int)coordinate->y == y && (int)coordinate->x == x)
+	// 			put_square(mlx, coordinate->x, coordinate->y, 1);
+	// 		else if (config->map[y][x] == '1')
+	// 			put_square(mlx, x, y, 0);
+	// 		x++;
+	// 	}
+	// 	y++;
+	// }
 	dda(data);
 	// put_vectors(data);
-	put_one_vector(data, data->player->angle, 0x00FF00);
+	// put_one_vector(data, data->player->angle, 0x00FF00);
 	// printf("player->coordinate->x %f\n", data->player->coordinate->x);
 	// printf("player->coordinate->y %f\n", data->player->coordinate->y);
 	// for (int i = 0; i < data->r_count; i++)
