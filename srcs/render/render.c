52b832b1e071fa3c393f0706549e2c9892db2ddd
Theo Duprez <tduprez@z1r5p1.42lyon.fr>
2023-10-19 14:51:38 +0200

    Start new function put_img_to_img

--- srcs/render/render.c
+++ srcs/render/render.c
@@ -6,7 +6,7 @@
 /*   By: tduprez <tduprez@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 18:15:02 by tduprez           #+#    #+#             */
-/*   Updated: 2023/10/18 23:02:57 by tduprez          ###   ########lyon.fr   */
+/*   Updated: 2023/10/19 14:51:29 by tduprez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,30 @@
 
 static int map_longest_row(t_data *data);
 static void put_player_pixel(t_mlx *mlx, int x, int y, int color);
+static void put_map_pixel(t_mlx *mlx, int x, int y, int color);
+
+void	ft_put_image_to_image(t_data *data)
+{
+	printf("%f | %f\n", data->player->coordinate->y, data->player->coordinate->x);
+	printf("%d | %d\n", data->config->map_size, data->config->map_row);
+	for (int i = 0; i < data->mlx->line_length; i++)
+	{
+		printf("TEST\n");
+		data->mlx->window_addr[i] = data->mlx->mini_map_addr[i];
+	}
+
+	for (int y = 0; y < (data->config->map_size + 1) * 15; y++)
+	{
+		for (int x = 0; x < (data->config->map_row + 1) * 15; x++)
+		{
+			if ((y >= data->player->coordinate->y * 15 && y <= data->player->coordinate->y * 15 + 8) && (x >= data->player->coordinate->x * 15 && x <= data->player->coordinate->x * 15 + 8))
+				put_map_pixel(data->mlx, x, y , 0x00FF0000);
+			// else
+				// put_map_pixel(data->mlx, x, y, data->mlx->mini_map_addr);
+		}
+	}
+	return ;
+}
 
 void render(t_config *config)
 {
@@ -21,10 +45,13 @@ void render(t_config *config)
 
 	get_data_address(&data);
 	init_data(&data, config);
+	data.mlx->window_img = mlx_new_image(data.mlx->mlx, (config->map_row + 1) * 15, (config->map_size + 1) * 15);
 	mlx_hook(data.mlx->mlx_win, 17, 0, &event_close, data.mlx);
 	mlx_hook(data.mlx->mlx_win, 2, (1L << 0), &event_move, data.mlx);
 	render_minimap(&data, data.mlx, data.config);
-	render_player(&data, data.mlx, data.config, data.player->coordinate);
+	ft_put_image_to_image(&data);
+	mlx_put_image_to_window(data.mlx->mlx, data.mlx->mlx_win, data.mlx->window_img, 0, 0);
+	// render_player(&data, data.mlx, data.config, data.player->coordinate);
 	mlx_loop(data.mlx->mlx);
 	return;
 }
@@ -39,6 +66,10 @@ void render_minimap(t_data *data, t_mlx *mlx, t_config *config)
 	(config->map_row + 1) * 15, (config->map_size + 1) * 15);
 	mlx->mini_map_addr = mlx_get_data_addr(mlx->mini_map_img, \
 	&mlx->bits_per_pixel, &mlx->line_length, &mlx->endian);
+	mlx->window_img = mlx_new_image(mlx->mlx, \
+	(config->map_row + 1) * 15, (config->map_size + 1) * 15);
+	mlx->window_addr = mlx_get_data_addr(mlx->window_img, \
+	&mlx->bits_per_pixel, &mlx->line_length, &mlx->endian);
 	y = 0;
 	while (y < config->map_size)
 	{
@@ -51,7 +82,9 @@ void render_minimap(t_data *data, t_mlx *mlx, t_config *config)
 		}
 		y++;
 	}
-	return ;
+	ft_strlcpy(mlx->window_addr, mlx->mini_map_addr, ft_strlen(mlx->window_addr));
+	// mlx->window_addr = ft_strdup(mlx->mini_map_addr);
+	return;
 }
 
 void render_player(t_data *data, t_mlx *mlx, t_config *config, t_coordinate* c)
@@ -60,6 +93,7 @@ void render_player(t_data *data, t_mlx *mlx, t_config *config, t_coordinate* c)
 	int	y;
 
 	y = 0;
+	(void)c;
 	config->map_row = map_longest_row(data);
 	mlx->player_img = mlx_new_image(mlx->mlx, 8, 8);
 	mlx->player_addr = mlx_get_data_addr(mlx->player_img, \
@@ -74,9 +108,10 @@ void render_player(t_data *data, t_mlx *mlx, t_config *config, t_coordinate* c)
 		}
 		y++;
 	}
-	mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, mlx->mini_map_img, 0, 0);
-	mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, mlx->player_img, \
-	(c->x + 0.75) * 15, (c->y + 0.75) * 15);
+	ft_put_image_to_image(data);
+	// mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, mlx->mini_map_img, 0, 0);
+	// mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, mlx->player_img, 
+	// (c->x + 0.75) * 15, (c->y + 0.75) * 15);
 	mlx_destroy_image(mlx->mlx, mlx->player_img);
 	return ;
 }
@@ -109,3 +144,14 @@ static void put_player_pixel(t_mlx *mlx, int x, int y, int color)
 	*(unsigned int *)dst = color;
 	return ;
 }
+
+static void put_map_pixel(t_mlx *mlx, int x, int y, int color)
+{
+	char *dst;
+
+	dst = mlx->window_addr + (y * mlx->line_length + x * \
+	(mlx->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+	return ;
+}
+
